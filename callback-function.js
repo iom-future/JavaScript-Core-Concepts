766f92d04deb653da195c268ab907ef3df25ea1d
iom_future <isaacmoses11111@gmail.com>
2025-09-05 10:36:22 -1100

    add new file and edit note in callback-function

--- callback-function.js
+++ callback-function.js
@@ -1,31 +1,32 @@
-//FIRST METHOD OF EXECUTING A CALLBACK FUNCTION
-// function showCalBackFunc(callBackFunc){
-//   console.log(callBackFunc(3,4));
-// }
-//
-//you declare the call back function
-// function cb(a,b){
-//   return a + b;
-// }
-//you pass a reference of it as an argument in the main function
-//NOTE:you dont call it with () you pass its name(reference) only
-// showCalBackFunc(cb);
+/*FIRST METHOD OF EXECUTING A CALLBACK FUNCTION
+* you declare the call back function before passing it['s name] as an argument
+* */
+function showCalBackFunc(callBackFunc){
+  console.log(callBackFunc(3,4));
+}
+
+/*you declare the call back function*/
+function cb(a,b){
+  return a + b;
+}
+/*you pass a reference of it as an argument in the main function
+//NOTE:you dont call it with () you pass its name(reference) only*/
+showCalBackFunc(cb);
 
 //SECOND METHOD OF EXECUTING A CALLBACK FUNCTION
 /*When using a call back function
-you run(call) the call back function in the main function even before declaring it
-,but it has to have a parameter(name) in the main func
-in this case callBackFunc is the parameter(name) for the call back function
+you can run(call) the call back function in the main function even before declaring it
+,but it has to have a parameter[which will be its name if it's a nameless function] in the main func
+in this case callBackFunc is the name for the call back function
 in this case showCalBackFunc is the main function*/
-function showCalBackFunc(callBackFunc) {
-    //where you call a function is where/when you pass the arguments
+function showCalBackFunc2(callBackFunc) {
+
     //running call back func even before declaration
     console.log(callBackFunc(4, 5));
 }
- /*calling the main function
-this is where you can declare your callback function
- and pass it as an arguments to your main func, hence the name call back func
+ /*calling the main function;where you call a function is where/when you pass the arguments
+this is where you can declare your callback function and pass it as an arguments to your main func;hence the name call back func
 using this method of writing call back func, when declaring the call back func  it can be anonymous(nameless)*/
- showCalBackFunc(function(a,b){
+ showCalBackFunc2(function (a,b){
    return a + b;
 })

1d67ea347da3c919ca7bdd089576365fffab53ec
iom_future <isaacmoses11111@gmail.com>
2025-10-02 00:59:01 -1100

    add new notes

---
+++ OOP/prototypal-inheritance.js
@@ -0,0 +1,3 @@
+/*prototypal inheritance is just what allows an object to inherit properties a method from another object;
+* borrowing the functionality (properties, method, etc.) from the parent(prototype) to the child that's without manually written it into the child
+* prototypal inheritance */
